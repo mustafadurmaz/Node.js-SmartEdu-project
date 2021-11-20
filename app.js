5284a35dddddad26236809841b770a1e3350306b
“mustafadurmaz” <mdmz_001@hotmail.com>
2021-11-20 23:57:57 +0300

    Proje başlangıç dosyaları oluşturuldu

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.status(200).render("index",{
+    page_name:"index"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about",{
+    page_name:"about"
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} nolu port'da başlatıldı`);
+});
